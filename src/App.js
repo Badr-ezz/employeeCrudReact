1cf33a3651837b7c5514204857f25662da6c9f74
badr ezz <ezziyatibadr@gmail.com>
2024-12-06 18:05:13 +0100

    adding dashboard

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 import { useState , useEffect} from 'react';
 import axios from 'axios';
 import Header from './pages/header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/dashboard/Dashboard';
+import NoMatch from './pages/noMatch/NoMatch';
 
 function App() {
  
   return (
    <>
       <Header />
+      <Routes>
+        <Route path='/' element={<Dashboard/>} />
+        <Route path='*' element={<NoMatch/>} />
+      </Routes>
    </>
   );
 }
